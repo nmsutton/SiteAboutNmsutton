bd7bb1ed2797ff6bee0ba6a5fe212ed68917e253
Nate Sutton <natemsutton@yahoo.com>
2013-10-04 10:14:55 -0700

    Further comments

--- app/assets/javascripts/general_site.js
+++ app/assets/javascripts/general_site.js
@@ -1,3 +1,8 @@
+/*
+ * This function allows an iframe content container to automatically
+ * resize based on its content.  It eliminates the need for scrolling 
+ * in the iframe instead of in the whole page.
+ */
 function resizeIframe(obj) {
 	obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
 }
