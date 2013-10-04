ed0a463b0ae4119af91d846df027768f70334e4c
Nate Sutton <natemsutton@yahoo.com>
2013-10-04 09:02:34 -0700

    Further comments added

--- app/controllers/tag_details_controller.rb
+++ app/controllers/tag_details_controller.rb
@@ -1,3 +1,10 @@
+# Nate Sutton
+# Copyright 2013
+#
+# This class includes methods to accept a tag name and generate relevant work
+# experience content based on the tag name.  The results returned are seperated
+# into sections based on sections in the database.
+#
 class TagDetailsController < ApplicationController
   def tag_details
     $tagName = params[:parameterName]
@@ -25,23 +32,24 @@ class TagDetailsController < ApplicationController
     return @DescriptionsOfWorkCategoryWithTag
   end
 
+  # This method accepts any collection of descriptions relevant to a tag and
+  # organizes css tags to stylize the presentation of the results.  The prefered 
+  # css styling is handled externally but formatting to prepare for it is done here.
+  #
   def createTextBoxFormatting(workCategoryDetails)
     $textBoxFormatting = ""
-    $textBoxFormatting << "<form action='/tag_navigator/tag_details' method='post' id='TagLinkGroup'>
-    <input id='TagLinkName' name='TagLinkName' type='hidden' />"
     for tagCategoryEntryIndex in (0..workCategoryDetails.length - 1)
-    $textBoxFormatting << "<div class='rounded-corners outer-shadows TagRelevantWorkDescription'>
-    <div id='TextBoxHeader'>"
-    $textBoxFormatting << workCategoryDetails[tagCategoryEntryIndex][0][0].time_range
-    $textBoxFormatting << "</div>"
-    for tagEntryIndex in (1..workCategoryDetails[tagCategoryEntryIndex].length - 1)
-    $textBoxFormatting << "<div id='TextBoxBody'>"
-    $textBoxFormatting << workCategoryDetails[tagCategoryEntryIndex][tagEntryIndex][0].description
-    $textBoxFormatting << "</div>"
+      $textBoxFormatting << "<div class='rounded-corners outer-shadows TagRelevantWorkDescription'>
+      <div id='TextBoxHeader'>"
+      $textBoxFormatting << workCategoryDetails[tagCategoryEntryIndex][0][0].time_range
+      $textBoxFormatting << "</div>"
+      for tagEntryIndex in (1..workCategoryDetails[tagCategoryEntryIndex].length - 1)
+        $textBoxFormatting << "<div id='TextBoxBody'>"
+        $textBoxFormatting << workCategoryDetails[tagCategoryEntryIndex][tagEntryIndex][0].description
+        $textBoxFormatting << "</div>"
+      end
+      $textBoxFormatting << "</div>"
     end
-    $textBoxFormatting << "</div>"
-    end
-    $textBoxFormatting << "</form>"
     return $textBoxFormatting.html_safe
   end
 end
