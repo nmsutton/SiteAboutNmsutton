a743ac1d63dec72f8d0ef842d38d2d8ad22553d5
Nate Sutton <natemsutton@yahoo.com>
2013-10-07 07:23:20 -0700

    another test added

--- test/functional/work_category_content_controller_test.rb
+++ test/functional/work_category_content_controller_test.rb
@@ -5,6 +5,12 @@ class WorkCategoryContentControllerTest < ActionController::TestCase
     get :choose_work_category
     assert_response :success
   end
+
+  test "Check that a response is received successfully from accessing the projects page" do
+    get "projects"
+    assert_response(:success, "The projects page did not create a successful response when an
+                     attempt was made to reach it")
+  end
   
   test "Confirm unique tags group contains retreived content " do
     get :projects
