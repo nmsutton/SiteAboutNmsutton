725e8e97d6687afc5bf4bb04087eb83d6fede38f
Nate Sutton <natemsutton@yahoo.com>
2013-10-09 15:49:21 -0700

    Removed some extra code

--- test/functional/tag_details_controller_test.rb
+++ test/functional/tag_details_controller_test.rb
@@ -5,21 +5,4 @@ class TagDetailsControllerTest < ActionController::TestCase
     get :tag_details
     assert_response :success
   end
-  
-  #test "test 1" do
-    #attributes = {
-    #  :parameterName => "Linux"
-    #}
-    #@request.env['RAW_POST_DATA'] = attributes.to_json
-    #post(:tag_details)
-    #assert true
-    
-    #post :tag_details, :parameterName => 'Linux'
-    
-    #post :getWorkDetails, {rubyGroupTable => Project, dbGroupTable => 'projects', rubyTagAndGroupTable=>Projecttag, dbTagAndGroupTable=>'projecttags', tag=>'Linux'}
-    
-    #refute_nil($tagName)
-    
-    #refute_nil(@WorkCategoryWithTag)
-  #end
 end
